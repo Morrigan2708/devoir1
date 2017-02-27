70124b17c4d92b0c211edc17221bacbbce04cb16
Morrigan2708 <amaury.trassaert.1@ens.etsmtl.ca>
2017-02-27 15:17:58 -0500

    Update AjaxServlet.java

--- src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
+++ src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
@@ -1,1 +1,37 @@
+/*Servlet qui fait l’affichage de la liste des images, qui peut recevoir une requête AJAX pour l’affichage du fragment des détails.
+  Code modifié par Amaury Trassaert, dans le cadre du cours GTI525, hiver 2017
+ */
 
+package ca.etsmtl.gti525.devoir1;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.ImageIcon;
+
+public class AjaxServlet extends HttpServlet {
+	
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+			}
+
+			
+}
