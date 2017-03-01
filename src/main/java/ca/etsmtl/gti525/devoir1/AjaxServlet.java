b1273891e6f4a77bba8bc0733a931d1e5a8954e5
Morrigan2708 <amaury.trassaert.1@ens.etsmtl.ca>
2017-03-01 17:41:16 -0500

    Update AjaxServlet.java

--- src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
+++ src/main/java/ca/etsmtl/gti525/devoir1/AjaxServlet.java
@@ -6,6 +6,8 @@ package ca.etsmtl.gti525.devoir1;
 
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +16,7 @@ import javax.swing.ImageIcon;
 
 public class AjaxServlet extends HttpServlet {
 	private Collection collection = new Collection();
-	private static final long serialVersionUID = -8308514275340556375L;
+	private static final long serialVersionUID = -98738739879470379874L;
 	
 	public AjaxServlet(){
 		super();
